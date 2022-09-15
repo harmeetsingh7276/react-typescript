776ec2810915502df2870a6ea7004ab40ad20590
harmeet.gorowada <harmeet.gorowada@techbulls.com>
2022-09-15 13:08:24 +0530

    Arrays in typescript

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,38 @@
 import './App.css';
 import { Greet } from './component/Greet';
 import { Person } from './component/Person';
+import { PersonList } from './component/PersonList';
 
 function App() {
-  const personName={
-    first:"Harmeet Singh",
-    last:"Gorowada"
+  const personName = {
+    first: "Harmeet Singh",
+    last: "Gorowada"
   }
+  const nameList = [
+    {
+      first: "Harmeet Singh",
+      last: "Gorowada"
+    },
+    {
+      first: "Pawandeep Kaur",
+      last: "Gorowada"
+    },
+    {
+      first: "Charanjit Kaur",
+      last: "Gorowada"
+    },
+    {
+      first: "Dharampal Singh",
+      last: "Gorowada"
+    }
+  ]
   return (
     <div className="App">
       <Greet name="Harmeet" messageCount={100} isLoggedIn={true} />
-      <Person name={personName}/>
+      <Person name={personName} />
+      <PersonList names={nameList}/>
     </div>
   );
-}
+  }
 
 export default App;
