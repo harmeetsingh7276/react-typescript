219d9de2020b7e7dd7102e88518d0907fcdb59d4
harmeet.gorowada <harmeet.gorowada@techbulls.com>
2022-09-15 12:57:57 +0530

    Object prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 
 import './App.css';
 import { Greet } from './component/Greet';
+import { Person } from './component/Person';
 
 function App() {
+  const personName={
+    first:"Harmeet Singh",
+    last:"Gorowada"
+  }
   return (
     <div className="App">
       <Greet name="Harmeet" messageCount={100} isLoggedIn={true} />
+      <Person name={personName}/>
     </div>
   );
 }
