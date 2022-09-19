35c55d348cf0b4b9605beafeb3a53db2180c279f
harmeet.gorowada <harmeet.gorowada@techbulls.com>
2022-09-19 19:23:39 +0530

    Onchange Event

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import { Button } from './component/Button';
 import { Greet } from './component/Greet';
 import { Heading } from './component/Heading';
+import { Input } from './component/Input';
 import { Oscar } from './component/Oscar';
 import { Person } from './component/Person';
 import { PersonList } from './component/PersonList';
@@ -33,9 +34,10 @@ function App() {
   ]
   return (
     <div className="App">
-      <Button handleClick={(e,id)=>{
+      {/* <Button handleClick={(e,id)=>{
         console.log("Button Clicked",e,id)
-      }} />
+      }} /> */}
+      <Input value='Harmeet' handleChange={(event)=>console.log(event)} />
     </div>
   );
 }
