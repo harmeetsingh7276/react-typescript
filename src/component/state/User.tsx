a396b6730480bb04e85d3d7f9f33dc5c458d6b1a
harmeet.gorowada <harmeet.gorowada@techbulls.com>
2022-09-23 22:52:59 +0530

    to avoid using null datatype

--- src/component/state/User.tsx
+++ src/component/state/User.tsx
@@ -5,7 +5,7 @@ type AuthUser={
     email:string
 }
 export const User=()=>{
-    const[user,setUser]=useState<AuthUser | null>(null)
+    const[user,setUser]=useState<AuthUser>({} as AuthUser)
     const handleLogin = () => {
         setUser({
             name:"Harmeet",
@@ -13,14 +13,14 @@ export const User=()=>{
         })
      }
     const handleLogout = () => {
-        setUser(null)
+        setUser({} as AuthUser)
      }
     return (
         <div>
             <button onClick={handleLogin}>Login</button>
             <button onClick={handleLogout}>Logout</button>
-            <div>User name is {user?.name}</div>
-            <div>User email is {user?.email}</div>
+            <div>User name is {user.name}</div>
+            <div>User email is {user.email}</div>
         </div>
     )
 }
